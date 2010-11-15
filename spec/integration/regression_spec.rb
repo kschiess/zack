1664775c45005b1019d46ef311dfa49b3542a0fe
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-11-15 14:55:41 +0100

    . refactored the test

--- spec/integration/regression_spec.rb
+++ spec/integration/regression_spec.rb
@@ -1,31 +1,35 @@
 require 'spec_helper'
 
 describe "Regression: " do
+  def self.fork_server(server_class)
+    # Fork a server in the background
+    before(:each) { 
+      @pid = fork do
+        Zack::Server.new(
+          'regression', 
+          :server => BEANSTALK_CONNECTION, 
+          :simple => server_class
+        ).run
+      end
+    }
+    after(:each) { 
+      Process.kill('TERM', @pid) 
+      Process.waitpid(@pid)
+    }
+  end
+  let(:client) { Zack::Client.new(
+    'regression', 
+    :server => BEANSTALK_CONNECTION, 
+    :with_answer => [:reader]) 
+  }
+    
   describe "asynchronous long running message, followed by a short running reader message (bug)" do
     class Regression1Server
       def reader; 42; end
       def long_running; sleep 0.1 end
     end
     
-    # Fork a server in the background
-    before(:each) { 
-      @pid = fork do
-        Zack::Server.new(
-          'regression_1', 
-          :server => BEANSTALK_CONNECTION, 
-          :simple => Regression1Server
-        ).run
-      end
-    }
-    after(:each) { 
-      Process.kill('TERM', @pid) 
-    }
-
-    let(:client) { Zack::Client.new(
-      'regression_1', 
-      :server => BEANSTALK_CONNECTION, 
-      :with_answer => [:reader]) 
-    }
+    fork_server Regression1Server
 
     # Wait for the server to launch
     before(:each) { 
@@ -43,5 +47,8 @@ describe "Regression: " do
       client.long_running
       client.reader.should == 42
     end 
+  end
+  describe "server crash during a message that has an answer" do
+    
   end
 end
