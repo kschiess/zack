92c2c07bbbf67be74ab9ff9adfcb1dcd80c89ae0
Kaspar Schiess <kaspar.schiess@absurd.li>
2010-11-29 06:37:25 +0100

    .

--- lib/zack.rb
+++
@@ -1,7 +0,0 @@
-require 'beanstalk-client'
-require 'yaml'
-
-module Zack; end
-
-require 'zack/server'
-require 'zack/client'
--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-
-require 'zack'
-
-Spec::Runner.configure do |config|
-  config.mock_with :flexmock
-end
-
-BEANSTALK_CONNECTION = 'localhost:11300'
