c95339f28aa9fcced31b9fc6b39e8d4824120043
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-11-28 15:45:48 +0100

    ! Fixes specs
    New behavior on exception would leave a message on the queue. With this
    commit, the specs are isolated and the leftover request doesn't crash
    the next one.

--- spec/acceptance/server_runner_spec.rb
+++ spec/acceptance/server_runner_spec.rb
@@ -10,6 +10,17 @@ describe "Server#run" do
     end
   end
   
+  # Isolates the specs by clearing the 'server_run' tube before use.
+  before(:each) { 
+    conn = Beanstalk::Connection.new(BEANSTALK_CONNECTION, 'server_run')
+    
+    while conn.peek_ready
+      conn.reserve.delete
+    end
+    
+    conn.close
+  }
+  
   let(:server) { Zack::Server.new(
     'server_run', 
     :simple => RunServerRun,
